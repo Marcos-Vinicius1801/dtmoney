2d90bce4ba02766cd4442c54a6ecf60bdbef12b3
Marcos Vinicius <marcos.brasilino@fatec.sp.gov.br>
2022-01-27 23:47:09 -0300

    Add: Header styles, Dashboard and Summary components

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,15 +3,19 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 :root {
     --background: #f0f2f5;
+    
     --red: #e52e40;
-    --blue: #5429cc;
+    
+    --green: #33cc95;
 
+    --blue: #5429cc;
     --bluelight: #6933ff;
     
     --text-title: #363f5f;
     --text-body: #969cb3;
 
     --shape: #ffffff;
+    --white: #ffffff;
 }
 
 *{
