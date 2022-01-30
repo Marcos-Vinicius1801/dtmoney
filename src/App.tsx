a17ed8e6d1c076b35c65002fdf132696e058d206
Marcos Vinicius <marcos.brasilino@fatec.sp.gov.br>
2022-01-30 16:39:31 -0300

    Add: Axios and Modal component lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+
 import { GlobalStyle } from "./styles/global";
 
+
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModaisOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+      setIsNewTransactionModaisOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModaisOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={ handleOpenNewTransactionModal }/>
       <Dashboard />
+      <Modal 
+          isOpen={ isNewTransactionModalOpen }
+          onRequestClose={ handleCloseNewTransactionModal }>
+          <h2>Cadastrar Transação</h2>        
+      </Modal>
       <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import logo from '../../assets/logo.svg';
 
 import { Container, Content } from './styles';
 
-export function Header(){
-    return(
+interface HeaderProps{
+    onOpenNewTransactionModal : () => void;
+}
+export function Header({ onOpenNewTransactionModal }: HeaderProps){
+     return(
         <Container>
             <Content>
                 <img src={logo} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={ onOpenNewTransactionModal }>
                     Nova transação
                 </button>
             </Content>
